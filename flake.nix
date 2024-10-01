3d5619d5134453580ce29476d2dc5c4b801eb804
Technik <aniket.97.mane@gmail.com>
2024-10-02 01:08:48 +0530

    Development environment for project using Nix dev shell.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Dev environment shell for rusty_forth";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            gcc
+            rust-bin.beta.latest.default
+          ];
+
+	  MSG = "Welcome to Dev Environment !!";
+
+          shellHook = ''
+            echo $MSG
+	    cargo --version
+          '';
+        };
+      }
+    );
+}
